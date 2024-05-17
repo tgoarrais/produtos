2e03ba2a3383f5bade0a023ded3508e925e6835e
tgoarrais <tgo-arrais@hotmail.com>
2024-05-17 15:57:35 -0300

    Coluna "Category" não precisa ser traduzida, pois a categoria será tratada em inglês.

--- src/main/java/br/com/meva/service/TraduzProdutoService.java
+++ src/main/java/br/com/meva/service/TraduzProdutoService.java
@@ -4,14 +4,14 @@ import br.com.meva.model.Produto;
 import net.suuft.libretranslate.Language;
 import net.suuft.libretranslate.Translator;
 
+import java.math.BigDecimal;
+
 public class TraduzProdutoService {
     public void traduzir(Produto produto) {
         String novoNome = Translator.translate(Language.ENGLISH, Language.PORTUGUESE, produto.getName());
-        String novaCategoria = Translator.translate(Language.ENGLISH, Language.PORTUGUESE, produto.getCategory());
         String novaDescricao = Translator.translate(Language.ENGLISH, Language.PORTUGUESE, produto.getDescription());
 
         produto.setName(novoNome);
-        produto.setCategory(novaCategoria);
         produto.setDescription(novaDescricao);
 
     }
